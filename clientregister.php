7cf4d9283ca91c72b6776ec5b487c0983c842bf1
TawhidulIslamRupom <tislam192118@bscse.uiu.ac.bd>
2024-02-29 21:35:06 +0600

    client initial requirements added

---
+++ clientregister.php
@@ -0,0 +1,34 @@
+<?php
+include_once 'dbconnection.php';
+session_start();
+
+//all the fields 
+$email = $_POST['email'];
+$password = $_POST['password'];
+$name = $_POST['name'];
+$phonenumber = $_POST['phonenumber'];
+
+
+
+$filename = $_FILES["picture"]["name"];
+$tempname = $_FILES["picture"]["tmp_name"];
+
+$folder = "client/images/" . $filename;
+
+$sql = "insert into  clients(email  ,password,name,phonenumber,picture) 
+values('" . $email  . "' , '" . $password . "','" . $name . "','" . $phonenumber . "','" . $filename . "');";
+
+echo $sql;
+$res = $conn->query($sql);
+
+if ($res) {
+    move_uploaded_file($tempname, $folder);
+    $_SESSION['user'] = "client";
+    $_SESSION['email'] = $email;
+    header("Location:client");
+} else {
+    echo "<script>
+    alert('Email alreay exist!!!!');
+    window.location.href='index.php';
+    </script>";
+}
