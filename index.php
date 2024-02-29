ac569454854f52113cdb2fb39ecf5339004f8413
TawhidulIslamRupom <tislam192118@bscse.uiu.ac.bd>
2024-02-29 21:21:17 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php include 'header.php' ?>
+<!--Start Features Section	-->
+<section class="features-section">
+	<div class="container">
+		<div class="row top-features">
+
+			<div class="col-lg-4">
+				<div class="features-box">
+					<img src="images/icon.png" alt="">
+					<h3>Winning Award</h3>
+					<p>Yearly we recognize Excellence, across the world, of the best in the Legal Industry. They become our Top Elite Law Firms.</p>
+				</div>
+			</div>
+
+			<div class="col-lg-4">
+				<div class="features-box">
+					<img src="images/icon2.png" alt="">
+					<h3>Private</h3>
+					<p>We believe in privacy over quantity. Our site is known as the best privacy for both lawyer and client.</p>
+				</div>
+			</div>
+
+
+
+			<div class="col-lg-4">
+				<div class="features-box">
+					<img src="images/icon3.png" alt="">
+					<h3>Legal Protection</h3>
+					<p>We provide the best Legal Protection is Bangladesh. Our firm is always open if you need any legal protection.</p>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="row align-items-center">
+			<div class="col-lg-4">
+				<div class="left-head">
+					<h2>Best Law for everyone</h2>
+				</div>
+			</div>
+
+			<div class="col-lg-8">
+				<div class="right-text">
+					<p>The largest Law firm in Bangladesh like to call themselves “Big Law” firms because they have sections specializing on each category of legal work, which in the U.S. usually means mergers and acquisitions transactions, banking, and certain types of high-stakes corporate litigation. These firms rarely do plaintiffs’ personal injury work. Justice Society is one of the best full service law firm in Bangladesh.</p>
+					<p>Justice Society is a multinational law chamber in bangladesh. We handled a lots of cases outside of the country. We have lots of international lawyers like Barrister A.M. Masum, Sikder M Ibrahim, Craig Carter, Anil Changaroth and many more.</p>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+	</div>
+</section>
+<!--Start Features Section	-->
+
+
+
+<!--Start About Section	-->
+<section class="about-section" id="about-section-id">
+	<div class="container">
+		<div class="row  align-items-center">
+			<div class="col-lg-6">
+				<div class="about-text text-center">
+					<h2>About law </h2>
+					<p>Law, the discipline and profession concerned with the customs, practices, and rules of conduct of a community that are recognized as binding by the community. Enforcement of the body of rules is through a controlling authority.
+					</p>
+					<div class="signature">
+						<img src="images/signature.png" alt="">
+						<p>CEO & Founder Of Justice Society</p>
+					</div>
+
+				</div>
+			</div>
+
+
+			<div class="col-lg-6">
+				<div class="about-img">
+					<img src="images/about.jpg" alt="">
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+<!--End About Section	-->
+<?php include 'footer.php' ?>
