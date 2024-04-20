02336e4faae2ecda06e921464aa86af3483f3664
nazmulpro007 <nazmulprodhan007@gmail.com>
2024-04-20 12:27:05 +0600

    index file added

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php include 'topbar.php' ?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4 mt-3">
+        <img src="images/Admin.png" alt="fdsa" width="1100px" height="500px">
+    </div>
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<?php include 'footer.php' ?>
+
+
+<script>
+    $("#home").addClass("active");
+</script>
