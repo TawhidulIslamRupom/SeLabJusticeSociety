a1b37a3e2d0dd3270aa4890e39879c7b3df34cb9
TakwaAkterUrmi <takter191185@bscse.uiu.ac.bd>
2024-04-20 12:57:08 +0600

    quote editing functionalities added

---
+++ admin/editquotecore.php
@@ -0,0 +1,19 @@
+<?php
+include_once '../dbconnection.php';
+session_start();
+$idquote = $_POST['idquote'];
+$quote = $_POST['quote'];
+$writer = $_POST['writer'];
+$identity = $_POST['identity'];
+
+$quote = str_replace("'","`",$quote);
+
+$query = "update quotes set quote='" . $quote . "' , writer='" . $writer . "', identity='" . $identity . "' where quote='" . $idquote . "';";
+echo $query;
+$result = $conn->query($query);
+if ($result) {
+    echo "<script>
+    alert('updated!!!');
+    window.location.href='quote.php';
+    </script>";
+}
