a105533cbf343c8fa053128c3f42d14db68cf39f
nazmulpro007 <nazmulprodhan007@gmail.com>
2024-04-20 13:10:07 +0600

    files for adding new quotes

---
+++ admin/addnewquotecore.php
@@ -0,0 +1,23 @@
+<?php
+include_once '../dbconnection.php';
+session_start();
+
+$quote = $_POST['quote'];
+$writer = $_POST['writer'];
+$identity = $_POST['identity'];
+$quote = str_replace("'","`",$quote);
+
+$query = "INSERT into quotes VALUES('" . $quote . "','" . $writer . "','" . $identity . "');";
+$result = $conn->query($query);
+if ($result) {
+    echo "<script>
+    alert('inserted!!!!');
+    window.location.href='quote.php';
+    </script>";
+} else {
+    echo "<script>
+    alert('Quote already exist, not inserted!');
+    window.location.href='quote.php';
+    </script>";
+ 
+}
